c0b505470c65a6c7cf92ceb80d5d423749df976b
Tommaso <polettitommaso@gmail.com>
2016-10-05 20:13:51 +0200

    Preparing the base file

---
+++ popup.js
@@ -0,0 +1,13 @@
+
+function getCurrentTabUrl(callback) {
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    var tab = tabs[0],
+         url = tab.url;
+
+    console.log( url );
+  });
