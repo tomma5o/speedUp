a95de1146211c2d373936c645417bbeb965eb6b3
Tommaso <polettitommaso@gmail.com>
2016-10-08 02:48:22 +0200

    Finish style
    Ended the html and css

--- popup.js
+++ popup.js
@@ -1,13 +1,48 @@
 
-function getCurrentTabUrl(callback) {
-  var queryInfo = {
-    active: true,
-    currentWindow: true
+document.addEventListener('DOMContentLoaded', function() {
+
+  function getCurrentTabUrl () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      var tab = tabs[0],
+      url = tab.url;
+      console.log(url);
+    });
+  };
+  function  pushLanguage (lang) {
+    chrome.tabs.query({ active: true, currentWindow: true },  function(tabs) {
+      var    tab = tabs[0],
+      urlArr = tab.url.split("/");
+      updateUrl = urlArr[3] = lang,
+      newUrl = urlArr.join("/");
+      console.log(newUrl);
+      chrome.tabs.update(tab.id, {url: newUrl});
+    });
+  };
+  function  getCookie () {
+    chrome.tabs.query({ active: true, currentWindow: true },  function(tabs) {
+      var tab = tabs[0],
+      cookie = tab.cookies;
+
+      console.log(cookie);
+      //chrome.tabs.reload();
+    });
   };
 
-  chrome.tabs.query(queryInfo, function(tabs) {
-    var tab = tabs[0],
-         url = tab.url;
+//get cookies and change desktop to mobie and viceversa
+var switch_menu = document.getElementsByClassName("switch_menu")[0];
 
-    console.log( url );
-  });
+switch_menu.addEventListener("click", function (e) {
+  var languageClass = e.target.getAttribute("id");
+  console.log(e.target)
+});
+
+// get class of ul an put in pushLanguage function
+var language_menu = document.getElementsByClassName("language_menu")[0];
+
+language_menu.addEventListener("click", function (e) {
+  var languageClass = e.target.getAttribute("class");
+  pushLanguage(languageClass.toLowerCase());
+});
+
+
+}, false);
